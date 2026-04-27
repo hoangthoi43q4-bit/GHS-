43a8a16a60f16c868da511c5d77bb1d89647ade0
uif45761 <yu.2.xia@continental-corporation.com>
2026-04-27 10:33:15 +0800

    UI change

---
+++ testDemo/Models/ProjectInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHPHandShake.Models
+{
+    /// <summary>
+    /// 第一层：项目信息 (对应 Excel 第四列)
+    /// </summary>
+    public class ProjectInfo
+    {
+        public string ProjectName { get; set; }
+
+        // 每个项目下包含多个位置/类型
+        public ObservableCollection<MaterialType> MaterialTypes { get; set; }
+
+        public ProjectInfo()
+        {
+            MaterialTypes = new ObservableCollection<MaterialType>();
+        }
+    }
+}
